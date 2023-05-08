496357ea11213e91ff3620278d941db802424f78
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-05-09 09:03:09 +1000

    Step 7: Apply emojiStyles to spans

--- src/Game.jsx
+++ src/Game.jsx
@@ -75,11 +75,11 @@ function Game() {
             {playerChoice && codeyChoice && (
                 <div>
                     <div style={choiceStyles}>
-                        <span>{playerChoice.emoji}</span>
+                        <span style={emojiStyles}>{playerChoice.emoji}</span>
                         <p>You chose {playerChoice.name}</p>
                     </div>
                     <div>
-                        <span>{codeyChoice.emoji}</span>
+                        <span style={emojiStyles}>{codeyChoice.emoji}</span>
                         <p>The computer chose {codeyChoice.name}</p>
                     </div>
                     <h2>{result}</h2>
